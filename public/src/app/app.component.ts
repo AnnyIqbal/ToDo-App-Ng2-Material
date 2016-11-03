04aaebd8f15d7bea342b09f87e0dc9e452cd9262
Anny <quratulann_iqbal@hotmail.com>
2016-11-03 12:00:32 +0500

    applies signout via angularfire2 Auth

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AngularFire } from 'angularfire2';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +9,23 @@ import { AngularFire } from 'angularfire2';
 })
 export class AppComponent {
   isSignedIn: boolean;
-   constructor(private af: AngularFire){
-     this.isSignedIn = false;
-   }
-    Signin(user, code) {
-      this.af.auth.login({ email: 'admin@todoapp.com', password: 'password'});
-      alert("done");
-      this.isSignedIn = true;
-    }
+  constructor(private af: AngularFire){
+    this.isSignedIn = false;
+  }
+  Signin(user, code) {
+    // this.af.auth.login();
+    this.af.auth.login(
+      {email: user , password: code},
+      {provider: AuthProviders.Password, method: AuthMethods.Password}
+    ).then((res)=>{
+        console.log(res);
+        this.isSignedIn = true;
+    },(err)=>{
+        console.log(err);
+    });
+  }
+  SignOut() {
+    this.af.auth.logout();
+    this.isSignedIn = false;
+  }
 }
