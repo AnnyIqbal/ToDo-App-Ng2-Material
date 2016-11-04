4bc6880edd33025f11df0eb56f5490093025e8b5
Anny <quratulann_iqbal@hotmail.com>
2016-11-04 15:59:18 +0500

    implements routing properly :D needs to improve ui

--- public/src/app/signin/signin.component.ts
+++ public/src/app/signin/signin.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFire } from 'angularfire2';
 import { AngularFireModule, AuthProviders, AuthMethods, FirebaseAuthState } from 'angularfire2';
 import { FirebaseAuth } from 'angularfire2/auth';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
@@ -11,7 +11,7 @@ import { FirebaseAuth } from 'angularfire2/auth';
 export class SigninComponent {
     isSignedIn: boolean;
 
-  constructor(private af: AngularFire){
+  constructor(private af: AngularFire, private route: Router){
     this.isSignedIn = false;
   }
 
@@ -23,6 +23,7 @@ export class SigninComponent {
         ).then((res)=>{
             alert("Sign In Successful!");
             this.isSignedIn = true;
+            this.route.navigate(['tasklist']); //navigate to todoapp
         },(err)=>{
             alert(err);
         });
