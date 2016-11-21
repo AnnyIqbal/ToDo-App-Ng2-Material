1fde67622ccb0df1c36820f13418e0f54aebc793
Anny <quratulann_iqbal@hotmail.com>
2016-11-21 15:47:25 +0500

    formatting n typos

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -5,8 +5,8 @@ import { FirebaseAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
+  // , styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
